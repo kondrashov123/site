6528a20b019aacdce1352948d47902b5b08c63e4
kondrashov123 <63396439+kondrashov123@users.noreply.github.com>
2020-07-22 21:25:08 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function chancge() {
+	$(this).prev().toggleClass("active")}
+	$(function(){
+		$('div h2').click(function(){
+			$(this).next().slideToggle();
+			$(this).toggleClass('active');
+		})
+	} )
