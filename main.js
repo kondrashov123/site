17ba93c21faaa8e05ab8b176a00f6660e80fb63b
kondrashov123 <63396439+kondrashov123@users.noreply.github.com>
2020-07-23 08:41:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 function chancge() {
 	$(this).prev().toggleClass("active")}
 	$(function(){
-		$('div h2').click(function(){
+		$('#div > h2').click(function(){
 			$(this).next().slideToggle();
 			$(this).toggleClass('active');
 		})
